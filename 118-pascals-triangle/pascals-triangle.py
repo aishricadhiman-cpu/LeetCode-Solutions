5bfdac864195086aa2ce5e0e3153979ab852ff91
Aishrica Dhiman <aishricadhiman@gmail.com>
2026-04-16 18:11:54 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (34.91%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.py
+++ 118-pascals-triangle/pascals-triangle.py
@@ -7,7 +7,7 @@ class Solution(object):
         result = []
         for i in range(0,numRows):
             rows = [1]*(i+1)
-            if i>0:
+            if i>1:
                 prev_rows = result[i-1]
                 for j in range(1,i):
                     rows[j] = prev_rows[j-1]+ prev_rows[j]
