c3f631037c504652a7545426b91e1c55c03097c5
Aishrica Dhiman <aishricadhiman@gmail.com>
2026-05-01 11:42:09 +0530

    Time: 603 ms (28.84%) | Memory: 0B (100.00%) - LeetSync

--- 1182-game-play-analysis-iv/game-play-analysis-iv.sql
+++ 1182-game-play-analysis-iv/game-play-analysis-iv.sql
@@ -1,13 +1,8 @@
-SELECT 
-    ROUND(
-        COUNT(DISTINCT a.player_id) / 
-        (SELECT COUNT(DISTINCT player_id) FROM Activity), 
-    2) AS fraction
-FROM Activity a
-JOIN (
-    SELECT player_id, MIN(event_date) AS first_login
-    FROM Activity
-    GROUP BY player_id
-) b
-ON a.player_id = b.player_id
-AND a.event_date = DATE_ADD(b.first_login, INTERVAL 1 DAY);
+    SELECT ROUND(COUNT(DISTINCT a.player_id)/COUNT(DISTINCT b.player_id),2) as fraction 
+    FROM (
+        SELECT player_id, MIN(event_date) AS first_login,
+        MIN(event_date) + INTERVAL 1 DAY AS expected_day 
+        FROM Activity 
+        GROUP BY player_id) b
+        LEFT JOIN Activity a ON a.player_id = b.player_id 
+        AND a.event_date = b.expected_day
